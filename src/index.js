c6fd7d4bb5e67f61ede115887756c4e75ec43cd6
rriver52 <rriver52>
2022-03-27 21:38:51 -0500

    Added Express.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello There')
+import express from "express";
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req,res) =>{
+    res.send({ msg: 'Hello There'})
+})
+
+app.listen(port)
