9ac2128d1d4cbad5000d02836a9e91c5be0906a5
rriver52 <rriver52>
2022-04-03 20:54:53 -0500

    New

--- src/utils/config.js
+++ src/utils/config.js
@@ -11,6 +11,9 @@ const envSchema = joi
     NODE_ENV: joi.string().valid('development', 'production').required(),
     PORT: joi.number().positive().required(),
     ORIGIN: joi.string().uri().required(),
+    DATABASE_URL: joi.string().uri().required(),
+    API_PASS: joi.string().required(),
+    API_USER: joi.string().required(),
   })
   .unknown()
 
@@ -22,4 +25,10 @@ if (error) {
   logger.log.error(new Error(`Config validation error: ${error.message}`))
 }
 
-export default { nodeEnv: env.NODE_ENV, port: env.PORT, origin: env.ORIGIN }
+export default {
+  nodeEnv: env.NODE_ENV,
+  port: env.PORT,
+  origin: env.ORIGIN,
+  apiUser: env.API_USER,
+  apiPass: env.API_PASS,
+}
